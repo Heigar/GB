62ee714a15caba23e4696e487d4f8310475802f2
Heigar <heigardev@yandex.ru>
2022-05-31 22:57:22 +0300

    HW4 day1

---
+++ src/JavaLevel1/HomeWorks_L1/HomeWork4/HomeWork4.java
@@ -0,0 +1,45 @@
+package JavaLevel1.HomeWorks_L1.HomeWork4;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+
+    }
+
+    public static char[] [] map;
+    public static final int SIZE = 3;
+    public static final int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '*';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+
+
+}
---
+++ src/JavaLevel1/Lessons_L1/Lesson4/Lesson4.java
@@ -0,0 +1,8 @@
+package JavaLevel1.Lessons_L1.Lesson4;
+
+public class Lesson4 {
+    public static void main(String[] args) {
+
+    }
+
+}
