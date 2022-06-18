ddf08dda3d4be4498dd800fc5244378561388394
Heigar <heigardev@yandex.ru>
2022-06-18 22:52:47 +0300

    lesson 5

---
+++ src/JavaLevel1/HomeWorks_L1/HomeWork5/HomeWork5.java
@@ -0,0 +1,4 @@
+package JavaLevel1.HomeWorks_L1.HomeWork5;
+
+public class HomeWork5 {
+}
---
+++ src/JavaLevel1/Lessons_L1/Lesson5/Lesson5.java
@@ -0,0 +1,7 @@
+package JavaLevel1.Lessons_L1.Lesson5;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+
+    }
+}
